01bdf476308aa81499e64ab0f809d2edfe64dbf8
Fernando L. Porfirio <fernando.lporfirio@gmail.com>
2025-08-06 12:16:44 -0300

    feat(migration-session): add max level field

--- apps/migrations/migrations/20250805213321_session.js
+++ apps/migrations/migrations/20250805213321_session.js
@@ -2,6 +2,7 @@ exports.up = function(knex) {
   return knex.schema.createTable('session', function(table) {
     table.increments('id').primary('pk_session');
     table.string('name').notNullable();
+    table.integer('max_level').nullable();
     table.integer('game_master_id').unsigned().notNullable();
     table.foreign('game_master_id').references('id').inTable('game_master');
     table.timestamp('started_at').nullable();
