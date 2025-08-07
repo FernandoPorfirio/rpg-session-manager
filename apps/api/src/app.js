40285d96bcac9c93121d39e91722e43cbdd10d0f
Fernando L. Porfirio <fernando.lporfirio@gmail.com>
2025-08-07 00:53:25 -0300

    feat: add base of api files

---
+++ apps/api/src/app.js
@@ -0,0 +1,21 @@
+require("module-alias/register");
+
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const app = express();
+const routes = require("./routes");
+const errorHandler = require('@middlewares/errorHandler');
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.use("/", routes);
+
+
+app.use(errorHandler);
+module.exports = app;
