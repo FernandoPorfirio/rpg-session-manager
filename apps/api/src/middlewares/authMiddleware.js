af5a400e86ce75441e352ae57eb05faa26c35162
Fernando L. Porfirio <fernando.lporfirio@gmail.com>
2025-08-07 02:41:10 -0300

    refactor

--- apps/api/src/middlewares/authMiddleware.js
+++ apps/api/src/middlewares/authMiddleware.js
@@ -1,17 +1,20 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization;
-  if (!authHeader) return res.status(401).json({ error: 'Token não fornecido' });
+  if (!authHeader)
+    return res.status(401).json({ error: "Token não fornecido" });
 
-  const [, token] = authHeader.split(' ');
+  const [, token] = authHeader.split(" ");
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
+
+    req.gameMaster = decoded;
+
     next();
   } catch {
-    return res.status(401).json({ error: 'Token inválido ou expirado' });
+    return res.status(401).json({ error: "Token inválido ou expirado" });
   }
 };
 
