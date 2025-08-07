176c7edb5ad65b9392fb22a8703ced6658c1def2
Fernando L. Porfirio <fernando.lporfirio@gmail.com>
2025-08-07 01:25:03 -0300

    feat: add login method

--- apps/api/src/controller/gameMasterController.js
+++ apps/api/src/controller/gameMasterController.js
@@ -1,14 +1,25 @@
-const contentUseCase = require("@useCase/gameMasterUseCase");
+const gameMasterUseCase = require("@useCase/gameMasterUseCase");
 
 const create = async (req, res) => {
   const { name, email, password } = req.body;
-
-    //TODO: FAZER O TRATAMENTO PADRAO PARA SENHA
-    const gameMaster = await contentUseCase.create({ name, email, password });
-
-    res.status(201).json(gameMaster);
+  const gameMaster = await gameMasterUseCase.create({ name, email, password });
+  res.status(201).json(gameMaster);
 };
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const token = await gameMasterUseCase.getToken({ email, password });
+  return res.json({ token });
+};
+
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const gameMaster = await gameMasterUseCase.getById({ id });
+  res.status(201).json(gameMaster);
+}
 
 module.exports = {
   create,
+  login,
+  getById
 };
