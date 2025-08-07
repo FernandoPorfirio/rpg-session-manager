3d979556be20dec3d58a79e81085597ec54c8612
Fernando L. Porfirio <fernando.lporfirio@gmail.com>
2025-08-07 02:40:37 -0300

    refactor: change folder name

---
+++ apps/api/src/controllers/gameMasterController.js
@@ -0,0 +1,25 @@
+const gameMasterUseCase = require("@useCase/gameMasterUseCase");
+
+const create = async (req, res) => {
+  const { name, email, password } = req.body;
+  const gameMaster = await gameMasterUseCase.create({ name, email, password });
+  res.status(201).json(gameMaster);
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const token = await gameMasterUseCase.getToken({ email, password });
+  return res.json({ token });
+};
+
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const gameMaster = await gameMasterUseCase.getById({ id });
+  res.status(201).json(gameMaster);
+}
+
+module.exports = {
+  create,
+  login,
+  getById
+};
