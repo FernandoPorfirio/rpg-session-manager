7c00dce1a9c207f185628286eb57888042d1a16e
Fernando L. Porfirio <fernando.lporfirio@gmail.com>
2025-08-07 01:26:25 -0300

    feat: add get token user case

--- apps/api/src/useCase/gameMasterUseCase.js
+++ apps/api/src/useCase/gameMasterUseCase.js
@@ -1,18 +1,64 @@
-const AppError = require('@errors/AppError');
+const AppError = require("@errors/AppError");
+
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 const gameMasterService = require("@service/database/gameMasterService");
 
 const create = async ({ name, email, password }) => {
+  const hashedPassword = await bcrypt.hash(password, 10);
+
   const databaseGameMaster = await gameMasterService.getByEmail(email);
 
-  console.log('databaseGameMaster',databaseGameMaster);
+  console.log("databaseGameMaster", databaseGameMaster);
 
   if (databaseGameMaster) {
-    throw new AppError('Email já cadastrado!', 400);
+    throw new AppError("Email já cadastrado!", 400);
   }
 
-  return await gameMasterService.create({ name, email, password });
+  return await gameMasterService.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+};
+
+const getToken = async ({ email, password }) => {
+  const gameMaster = await gameMasterService.getByEmail(email);
+
+  if (!gameMaster) {
+    throw new AppError("Game Master não encontrado!", 404);
+  }
+
+  const validPassword = await bcrypt.compare(password, gameMaster.password);
+
+  if (!validPassword) {
+    throw new AppError("Senha inválida!", 401);
+  }
+
+  const token = jwt.sign(
+    { id: gameMaster.id, email: gameMaster.email },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1h",
+    }
+  );
+
+  return token;
+};
+
+const getById = async ({ id }) => {
+  const gameMaster = await gameMasterService.getById(id);
+
+  if (!gameMaster) {
+    throw new AppError("Game Master não encontrado!", 404);
+  }
+
+  return gameMaster;
 };
 
 module.exports = {
   create,
+  getToken,
+  getById
 };
