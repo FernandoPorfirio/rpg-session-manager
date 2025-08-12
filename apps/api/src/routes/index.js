ff93533c4c8eff29075f460642b3cf899f51f9df
Fernando L. Porfirio <fernando.lporfirio@gmail.com>
2025-08-11 22:00:23 -0300

    refactor: add session routes to the main router

--- apps/api/src/routes/index.js
+++ apps/api/src/routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const healthcheckRoutes = require("./healthcheckRoute");
 const gameMasterkRoutes = require("./gameMaster/gameMasterRoute");
+const sessionRoutes = require("./session/sessionRoute");
 
 router.use("/healthcheck", healthcheckRoutes);
 router.use("/game_master", gameMasterkRoutes);
+router.use("/session", sessionRoutes);
 
 module.exports = router;
