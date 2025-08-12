898c3fd67a38653b5d0241dd23f89c1a514006cc
Fernando L. Porfirio <fernando.lporfirio@gmail.com>
2025-08-11 22:27:09 -0300

    feat: implement player use case with CRUD operations

---
+++ apps/api/src/useCase/playerUseCase.js
@@ -0,0 +1,60 @@
+const AppError = require("@errors/AppError");
+const playerService = require("@service/database/playerService");
+
+const create = async ({ name, classId, level, lore, gameMasterId }) => {
+  return await playerService.create({
+    name,
+    classId,
+    level,
+    lore,
+    gameMasterId
+  });
+};
+
+const update = async ({ id, name, classId, level, lore }) => {
+  const currentPlayer = await playerService.getById(id);
+
+  if (!currentPlayer) {
+    throw new AppError("Player não encontrado!", 404);
+  }
+
+  return await playerService.update({
+    id,
+    name,
+    classId,
+    level,
+    lore
+  });
+};
+
+const getById = async ({ id }) => {
+  const player = await playerService.getById(id);
+
+  if (!player) {
+    throw new AppError("Player não encontrado!", 404);
+  }
+
+  return player;
+};
+
+const getByGameMasterIdWithFilters = async ({ gameMasterId, sessionId, guildId }) => {
+  return await playerService.getByGameMasterIdWithFilters(gameMasterId, { sessionId, guildId });
+};
+
+const deletePlayer = async ({ id }) => {
+  const player = await playerService.getById(id);
+
+  if (!player) {
+    throw new AppError("Player não encontrado!", 404);
+  }
+
+  return await playerService.deleteById(id);
+};
+
+module.exports = {
+  create,
+  update,
+  getById,
+  getByGameMasterIdWithFilters,
+  deletePlayer
+};
